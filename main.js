cf25071384edef7bd8edf0530df0f02e1f7e7dd9
Ben8842 <z87e99b44r55a@gmail.com>
2020-07-15 01:08:03 -0400

    Added a function for the sortable list. Created the main page coin list card and created the coin list html minor changes to headers

--- main.js
+++ main.js
@@ -507,7 +507,6 @@ $.getJSON("https://coinlib.io/api/v1/global?key=56a2275998bf3767", function (
 
 //This is a function that uses axios.  It populates the Top One Hundred page
 getCoinList = () => {
-  //let movieId = sessionStorage.getItem("movieId");
   axios
     .get(
       `https://coinlib.io/api/v1/coinlist?key=56a2275998bf3767&page=1&order=rank_asc`
@@ -550,6 +549,50 @@ getCoinList = () => {
     .catch(error => {
       console.log(error);
     });*/
+};
+
+//This Function is necessary to trigger the sortable list for the Coin List page
+$(document).ready(function () {
+  $("#table_id").DataTable();
+});
+//This Function populates the Coin List page
+getCoinListSort = () => {
+  axios
+    .get(
+      `https://coinlib.io/api/v1/coinlist?key=56a2275998bf3767&page=1&order=rank_asc`
+    )
+
+    .then((response) => {
+      console.log(response.data);
+      // console.log(response.data.coins[0]);
+      let output = "";
+      let superCurrency = response.data.coins;
+      var y = 0;
+      $.each(superCurrency, (index, coinBucket) => {
+        y++;
+        var pricef = Math.floor(coinBucket.price * 100) / 100;
+        var market_capf = Math.floor(coinBucket.market_cap * 100) / 100;
+        var pricefc = pricef.toLocaleString();
+        var market_capfc = market_capf.toLocaleString();
+        var delta_24hf = Math.floor(coinBucket.delta_24h * 100) / 100;
+        var volume_24hf = Math.floor(coinBucket.volume_24h * 100) / 100;
+        var delta_24hfc = delta_24hf.toLocaleString();
+        var volume_24hfc = volume_24hf.toLocaleString();
+        output += `
+          <tr>
+          <th scope="row">${y}</th>
+          <td>${coinBucket.name}</td>
+          <td>${coinBucket.symbol}</td>
+          <td>$ ${pricefc}</td>
+          <td>% ${delta_24hfc}</td>
+          <td>$ ${market_capfc}</td>
+          <td>$ ${volume_24hfc}</td>
+          </tr>
+          `;
+      });
+
+      $("#superCoinPlace").html(output);
+    });
 };
 
 //This function below 'detailsFunction' is currently not used
