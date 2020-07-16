5fc8a428572629c330a0751cb75b610bdaa314e0
Ben8842 <z87e99b44r55a@gmail.com>
2020-07-16 00:27:18 -0400

    I added an option variable to the chart on the build page which allows the doughnut/pie to display percentages on the actual pie.  Also, other display changes for coinlist

--- main.js
+++ main.js
@@ -148,6 +148,26 @@ const myBuildFunction = () => {
         "Total Portfolio value = $" + totalC.toLocaleString()
       );
 
+      var options = {
+        tooltips: {
+          enabled: false,
+        },
+        plugins: {
+          datalabels: {
+            formatter: (value, ctx) => {
+              let sum = 0;
+              let dataArr = ctx.chart.data.datasets[0].data;
+              dataArr.map((data) => {
+                sum += data;
+              });
+              let percentage = ((value * 100) / sum).toFixed(2) + "%";
+              return percentage;
+            },
+            color: "#000",
+          },
+        },
+      };
+
       //Here is where we are building the chart
       var ctx = document.getElementById("buildChart").getContext("2d");
       var chart = new Chart(ctx, {
@@ -190,7 +210,7 @@ const myBuildFunction = () => {
         },
 
         // Configuration options go here
-        options: {},
+        options: options,
       });
       /*  not sure how to get this addData function to work
         function addData(){
